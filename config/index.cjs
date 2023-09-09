b6593dc7bb8b7154610a7f4523e9338678f277fd
daimengmeng <80146072+mmdai-blcc@users.noreply.github.com>
2023-09-09 11:29:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -97,7 +97,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oByAH6V4iWvnxlMrK3cfcbvrhFS0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'IKlq2L1mebqUVAvcLKMbGdN_cTN9q3DFpUjv4i8bL4U',
+      useTemplateId: 'rS8-5pBxcwb_9RDGHT--GeyZqq8pzagBvp0vGgQqy14',
       
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-03',
@@ -149,7 +149,11 @@ const USER_CONFIG = {
             // 周四
             [
               '13:00-15:45 翻译学概论'
-            ]
+            ],
+            // 周六
+            [
+              '18:00-20:45 英美诗歌研究'
+            ],
           ],
           // 双周课表
           even: [
